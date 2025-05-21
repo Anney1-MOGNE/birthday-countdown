1dba981330259ed94ff2234a6c80c7b617a6fffd
Anney1-MOGNE <anecmhlambi@gmail.com>
2025-05-22 01:22:35 +0200

    Added PWA support 🚀

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+      caches.open("birthday-cache").then((cache) => {
+        return cache.addAll([
+          "./",
+          "./index.html",
+          "./manifest.json",
+          "./style.css",
+          "./script.js",
+          "./icon-192.png",
+          "./icon-512.png"
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", (e) => {
+    e.respondWith(
+      caches.match(e.request).then((response) => {
+        return response || fetch(e.request);
+      })
+    );
+  });
